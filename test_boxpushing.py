574d9f20d4ad0cf5bdc51e263d905d50bcfbb303
Ericonaldo <ericliuof97@gmail.com>
2022-10-18 15:50:24 +0800

    add box pushing env description

--- test_boxpushing.py
+++ test_boxpushing.py
@@ -3,13 +3,13 @@ from minigrid.wrappers import RGBImgObsWrapper, ImgObsWrapper
 import cv2
 
 env = gym.make('MiniGrid-BoxPushing-16x16-v0')
-env = RGBImgObsWrapper(env, highlight=False) # Get pixel observations
+env = RGBImgObsWrapper(env, tile_size=64, highlight=False) # Get pixel observations
 env = ImgObsWrapper(env) # Get rid of the 'mission' field
 obs, _ = env.reset() # This now produces an RGB tensor only
 
 print(obs.shape)
 
-cv2.imwrite("image.jpg", obs)
-cv2.imshow("image", obs)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv2.imwrite("boxpushing-env.png", obs)
+# cv2.imshow("image", obs)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
